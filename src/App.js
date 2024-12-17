533cd1f44cf846ba8815ca6688af2d3f04599b86
Ramber Abdul Malik P. <157560203+ramber322@users.noreply.github.com>
2024-12-18 03:16:20 +0800

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
+import Register from "./Register";
+
+//USER
 import ViewCalendar from "./ViewCalendar";
 import Dashboard from "./Dashboard";
+import ActivityLog from "./ActivityLog";
+
+//ADMIN
+import AddEvent from "./ADMIN/AddEvent";
+import Feedback from "./ADMIN/Feedback";
+import AdminDashboard from "./ADMIN/AdminDashboard";
+
 
 import ProtectedRoute from "./ProtectedRoute";
 
@@ -10,28 +20,32 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Public route for login */}
+
+        {/* Public routes */}
         <Route path="/" element={<Login />} />
-
-        {/* Protected route for dashboard */}
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/register" element={<Register />} />
 
 
-          <Route
-          path="/viewcalendar"
-          element={
-            <ProtectedRoute>
-              <ViewCalendar />
-            </ProtectedRoute>
-          }
-        />
+
+        {/* Protected routes */}
+        <Route path="/dashboard"
+        element={ <ProtectedRoute> <Dashboard/> </ProtectedRoute> }/>
+
+        <Route path="/viewcalendar"
+        element={ <ProtectedRoute> <ViewCalendar/> </ProtectedRoute> }/>
+
+        <Route path="/activitylog"
+        element={ <ProtectedRoute> <ActivityLog/> </ProtectedRoute> }/>
+
+        <Route path="/addevent" 
+        element={ <ProtectedRoute> <AddEvent/> </ProtectedRoute> }/>
+
+        <Route path="/feedback" 
+        element={ <ProtectedRoute> <Feedback/> </ProtectedRoute> }/>
+
+        <Route path="/admindashboard" 
+        element={ <ProtectedRoute> <AdminDashboard/> </ProtectedRoute> }/>
+
 
       </Routes>
     </Router>
